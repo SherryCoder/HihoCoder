b894f1d7f5aa2312b2f4bdb1b59389e04aece243
SherryCoder <notsherry.sun@outlook.com>
2017-09-23 10:36:38 +0800

    1077SegmentTree

--- HihoCoder/1070RMQII.cpp
+++ HihoCoder/1070RMQII.cpp
@@ -1,81 +1,81 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-int N;
-const int MAX = 1e4+2;
-vector<int> weight;
-int tree_node[MAX][MAX];
-
-//用tree_node[i][j]表示在以i为左边界，长度为j的区间内的最小权值
-
-void build_dfs(int node,int len){
-	if(len == 1){
-		tree_node[node][len] = weight[node];
-		return;
-	}
-	build_dfs(node,len/2);
-	build_dfs(node+len/2,len - len/2);
-	tree_node[node][len] = min(tree_node[node][len/2],tree_node[node+len/2][len - len/2]);
-}
-
-int search_min(int s_L ,int s_len , int L , int len){
-	if(s_L == L && s_len == len){
-		return tree_node[s_L][s_len];	
-	}
-	if(L >= (s_L+s_len/2)){
-		return search_min(s_L+s_len/2,s_len-s_len/2,L,len);
-	}
-	else if((L+len-1) < (s_L+s_len/2)){
-		return search_min(s_L,s_len/2,L,len);
-	}else{
-		int l_len = s_L+s_len/2 - L;
-		int r_len = len - l_len;
-		int l_min = search_min(s_L,s_len/2,L,l_len);
-		int r_min = search_min(s_L+s_len/2,s_len-s_len/2,L+l_len,r_len);
-		int result = min(l_min,r_min);
-		return result;
-	}
-}
-
-void update(int s_L,int s_len,int pos,int value){
-	if(s_L == pos && s_len ==1){
-		tree_node[s_L][s_len] = value;
-		return;
-	}
-	if(pos < s_L+s_len/2){
-		update(s_L,s_len/2,pos,value);
-	}else{
-		update(s_L+s_len/2,s_len-s_len/2,pos,value);
-	}
-	tree_node[s_L][s_len] = min(tree_node[s_L][s_len/2],tree_node[s_L+s_len/2][s_len-s_len/2]);
-}
-
-int main(){
-	cin>>N;
-	weight.push_back(-1);
-	for(int i = 1;i <= N;i++){
-		int w;cin>>w;
-		weight.push_back(w);
-	}
-	int Q;
-	cin>>Q;
-	build_dfs(1,N);
-
-	while(Q){
-		int type;
-		cin>>type;
-		//表示一个询问
-		if(type == 0){
-			int L,R;
-			cin>>L>>R;
-			int minv = search_min(1,N,L,R-L+1);
-			cout<<minv<<endl;
-		}else{
-			int P,W;
-			cin>>P>>W;
-			update(1,N,P,W);
-		}
-		Q--;
-	}
-
-}
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//int N;
+//const int MAX = 1e4+2;
+//vector<int> weight;
+//int tree_node[MAX][MAX];
+//
+////用tree_node[i][j]表示在以i为左边界，长度为j的区间内的最小权值
+//
+//void build_dfs(int node,int len){
+//	if(len == 1){
+//		tree_node[node][len] = weight[node];
+//		return;
+//	}
+//	build_dfs(node,len/2);
+//	build_dfs(node+len/2,len - len/2);
+//	tree_node[node][len] = min(tree_node[node][len/2],tree_node[node+len/2][len - len/2]);
+//}
+//
+//int search_min(int s_L ,int s_len , int L , int len){
+//	if(s_L == L && s_len == len){
+//		return tree_node[s_L][s_len];	
+//	}
+//	if(L >= (s_L+s_len/2)){
+//		return search_min(s_L+s_len/2,s_len-s_len/2,L,len);
+//	}
+//	else if((L+len-1) < (s_L+s_len/2)){
+//		return search_min(s_L,s_len/2,L,len);
+//	}else{
+//		int l_len = s_L+s_len/2 - L;
+//		int r_len = len - l_len;
+//		int l_min = search_min(s_L,s_len/2,L,l_len);
+//		int r_min = search_min(s_L+s_len/2,s_len-s_len/2,L+l_len,r_len);
+//		int result = min(l_min,r_min);
+//		return result;
+//	}
+//}
+//
+//void update(int s_L,int s_len,int pos,int value){
+//	if(s_L == pos && s_len ==1){
+//		tree_node[s_L][s_len] = value;
+//		return;
+//	}
+//	if(pos < s_L+s_len/2){
+//		update(s_L,s_len/2,pos,value);
+//	}else{
+//		update(s_L+s_len/2,s_len-s_len/2,pos,value);
+//	}
+//	tree_node[s_L][s_len] = min(tree_node[s_L][s_len/2],tree_node[s_L+s_len/2][s_len-s_len/2]);
+//}
+//
+//int main(){
+//	cin>>N;
+//	weight.push_back(-1);
+//	for(int i = 1;i <= N;i++){
+//		int w;cin>>w;
+//		weight.push_back(w);
+//	}
+//	int Q;
+//	cin>>Q;
+//	build_dfs(1,N);
+//
+//	while(Q){
+//		int type;
+//		cin>>type;
+//		//表示一个询问
+//		if(type == 0){
+//			int L,R;
+//			cin>>L>>R;
+//			int minv = search_min(1,N,L,R-L+1);
+//			cout<<minv<<endl;
+//		}else{
+//			int P,W;
+//			cin>>P>>W;
+//			update(1,N,P,W);
+//		}
+//		Q--;
+//	}
+//
+//}
--- HihoCoder/1077SegmentTree.cpp
+++ HihoCoder/1077SegmentTree.cpp
@@ -1,31 +1,88 @@
-//#include<iostream>
-//#include<vector>
-//using namespace std;
-//int N;
-//vector<int> weight;
-//
-//int main(){
-//	cin>>N;
+#include<iostream>
+#include<vector>
+using namespace std;
+int N;
+const int MAX = 1e6+2;
+int weight[MAX];   //事先申请好这个数组，和用vector，vector会超时
+
+typedef struct node{
+	int l;		//区间的左边界
+	int r;		//区间的右边界
+	int minV;	//区间的最小值
+};
+node tree[2*MAX];    //对于一个节点i，他的左节点存在2*i，右节点存在2*i+1
+					 //为什么这边的数组大小是2*MAX,一个n的区间长度的线段树的节点个数是2*n-1，因此数组大小只需要2*MAX就够了
+
+void build(int l,int r,int pos){
+	tree[pos].l = l;
+	tree[pos].r = r;
+	if(l==r){
+		tree[pos].minV = weight[l];
+	}else{
+		build(l,(l+r)/2,pos*2);      //pos*2是pos的左孩子  //这边不是很理解
+		build((l+r)/2+1,r,pos*2+1);  //pos*2+1是pos的右孩子
+		tree[pos].minV = min(tree[pos*2].minV,tree[pos*2+1].minV);
+	}
+}
+
+int query(int l,int r,int pos){    //从节点pos开始查找区间[l,r]的最小值
+	if(tree[pos].l == l&& tree[pos].r == r){
+		return tree[pos].minV;	
+	}
+	int mid = (tree[pos].l + tree[pos].r)/2;
+	if(r<=mid){
+		return query(l,r,pos*2);
+	}else if(l>mid){
+		return query(l,r,pos*2+1);
+	}else{
+		int min_l = query(l,mid,pos*2);
+		int min_r = query(mid+1,r,pos*2+1);
+		int get_min = min(min_l,min_r);
+		return get_min;
+	}
+}
+
+void update(int pi,int vi,int pos){
+	//weight[pi] = vi;
+	if(tree[pos].l == pi && tree[pos].l == tree[pos].r){
+		tree[pos].minV = vi;
+	}else{
+		int mid = (tree[pos].l+tree[pos].r)/2;
+		if(pi <= mid){
+			update(pi,vi,pos*2);
+		}
+		else if(pi > mid){
+			update(pi,vi,pos*2+1);
+		}
+		tree[pos].minV = min(tree[2*pos].minV,tree[2*pos+1].minV);
+	}
+}
+
+int main(){
+	scanf("%d",&N);
 //	weight.push_back(-1);
-//	for(int i = 1;i <= N;i++){
-//		int w;cin>>w;
-//		weight.push_back(w);
-//	}
-//	int Q;
-//	cin>>Q;
-//	while(Q){
-//		int type;
-//		cin>>type;
-//		//表示一个询问
-//		if(type == 0){
-//			int L,R;
-//			cin>>L>>R;
-//
-//		}else{
-//			int P,W;
-//			cin>>P>>W;
-//
-//		}
-//	}
-//
-//}
+	for(int i = 1;i <= N;i++){
+		//int w;
+		scanf("%d",&weight[i]);
+		//weight.push_back(w);
+	}
+	build(1,N,1);
+	int Q;
+	scanf("%d",&Q);
+	while(Q){
+		int type;
+		scanf("%d",&type);
+		//表示一个询问
+		if(type == 0){
+			int L,R;
+			scanf("%d%d",&L,&R);
+			printf("%d\n",query(L,R,1));
+		}else{
+			int P,W;
+			scanf("%d%d",&P,&W);
+			update(P,W,1);
+		}
+		Q--;
+	}
+	return 0;
+}
